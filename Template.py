003f6c28d7d9e7eddb54eddb5a2a8c57c5ed90ce
bAe <bjekele@fexco.com>
2022-12-04 16:16:47 +0000

    Added Template

---
+++ Template.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Template.py
+    Author: Bissallah Ekele - bAe
+    Date: 
+    Description: 
+"""
+
+def get_part_1(input_list):
+    """Function description
+
+        Keyword argument:
+        input_list -- parameter description
+
+        Return:
+        returned value
+
+        Throws:
+        if exceptions are thrown
+    """
+    pass
+
+def get_part_2(input_list):
+    pass
+
+def main():
+    test_input = "test-input"
+    puzzle_input = "puzzle-input"
+
+    file_name = test_input
+
+    # with open(file_name) as f:
+    #     input_list = f.read().splitlines()
+
+    # print("Day_1 Part_1: " 
+    #     + str(get_part_1(input_list)) 
+    #     + "\nPart_2: "
+    #     + str(get_part_2(input_list)))
+
+if __name__ == "__main__":
+    main()
